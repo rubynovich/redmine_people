3af91a3de4dfe5f46803b81563eae60659317b1b
Maksim <max@koritskiy.ru>
2014-05-07 16:52:17 +0400

    fix add saitized phones in migration

--- db/migrate/20131107074108_add_sanitazed_phones_to_user.rb
+++ db/migrate/20131107074108_add_sanitazed_phones_to_user.rb
@@ -1,14 +1,17 @@
 class AddSanitazedPhonesToUser < ActiveRecord::Migration
 
-  def change
+  def up
 
     add_column(:users, :sanitized_phones, :string)
 
     for user in User.all
-      user.sanitized_phones = (user.phone || '').gsub(/[-()\s]/, '')
-      user.save
+      user.update_column(:sanitized_phones, (user.phone || '').gsub(/[-()\s]/, ''))
     end
 
+  end
+
+  def down
+    remove_column(:users, :sanitized_phones)
   end
 
 end
