797b196db411db12fb26ea87ccb00e63c5ddbcf6
Maksim <max@koritskiy.ru>
2014-05-14 15:17:40 +0400

    add support assets

--- lib/redmine_people/hooks/views_layouts_hook.rb
+++ lib/redmine_people/hooks/views_layouts_hook.rb
@@ -2,7 +2,7 @@ module RedminePeople
   module Hooks
     class ViewsLayoutsHook < Redmine::Hook::ViewListener
       def view_layouts_base_html_head(context={})
-        return stylesheet_link_tag(:people, :plugin => 'redmine_people')
+        return stylesheet_link_tag(:people, :plugin => 'redmine_people') unless defined?(RedminePluginAssetPipeline)
       end
     end
   end
